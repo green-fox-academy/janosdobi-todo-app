50b50c9f3e24f3f17e074652fb7dd9960bab1643
Janos Dobi <j.dobi88@gmail.com>
2017-09-28 10:23:08 +0200

    add Todo class w Print usage

---
+++ src/Todo.java
@@ -0,0 +1,15 @@
+public class Todo {
+
+    public Todo() {
+        System.out.println("Command Line Todo application");
+        System.out.println("=============================");
+        System.out.println("");
+        System.out.println("Command line arguments:");
+        System.out.println("-l   Lists all the tasks");
+        System.out.println("-a   Adds a new task");
+        System.out.println("-r   Removes a task");
+        System.out.println("-c   Completes a task");
+
+    }
+
+}
