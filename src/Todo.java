4818373cf6577bde34ffccef5f37429b85fc8700
Janos Dobi <j.dobi88@gmail.com>
2017-09-28 17:23:57 +0200

    argument exception handling

--- src/Todo.java
+++ src/Todo.java
@@ -4,18 +4,43 @@ public class Todo {
 
         if (args.length == 0) {
             TodoList myTodo = new TodoList();
-        }else if (args[0].startsWith("-l")) {
-            TodoList myTodo = new TodoList("input");
-            myTodo.getList();
+        } else if (args[0].startsWith("-l")) {
+            TodoList myTodo = new TodoList("-l");
+            try {
+                myTodo.getList();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unsupported argument");
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("Unsupported argument");
+            }
         } else if (args[0].startsWith("-a")) {
-            TodoList myTodo = new TodoList("input");
-            myTodo.setList(args[1]);
+            TodoList myTodo = new TodoList("-a");
+            try {
+                myTodo.setList(args[1]);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unsupported argument");
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("Unsupported argument");
+            }
         } else if (args[0].startsWith("-c")) {
-            TodoList myTodo = new TodoList("input");
-            myTodo.checkItem(args[1]);
+            TodoList myTodo = new TodoList("-c");
+            try {
+                myTodo.checkItem(args[1]);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unsupported argument");
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("Unsupported argument");
+            }
         } else if (args[0].startsWith("-r")) {
-            TodoList myTodo = new TodoList("input");
-            myTodo.removeItem(args[1]);
+            TodoList myTodo = new TodoList("-r");
+            try {
+                myTodo.removeItem(args[1]);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unsupported argument");
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("Unsupported argument");
+            }
         }
     }
 }
+
