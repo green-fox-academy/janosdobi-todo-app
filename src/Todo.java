beebd9d337cb47579b8494576f89cae199c305a5
Janos Dobi <j.dobi88@gmail.com>
2017-09-28 10:55:06 +0200

    add reading from file and writing into list

--- src/Todo.java
+++ src/Todo.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Todo {
 
+    //empty constructor prints out usage info
     public Todo() {
         System.out.println("Command Line Todo application");
         System.out.println("=============================");
@@ -9,7 +16,23 @@ public class Todo {
         System.out.println("-a   Adds a new task");
         System.out.println("-r   Removes a task");
         System.out.println("-c   Completes a task");
+        System.out.println("");
+    }
+
+
+    public void getList() {
+        Path path = Paths.get("src/Data.txt");
+        try {
+            List<String> myList = Files.readAllLines(path);
+            for (String item : myList) {
+                System.out.println(item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+
 
 }
